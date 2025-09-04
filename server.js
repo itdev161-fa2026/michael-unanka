0fcabcac15b56a904135b0e7d7bb8a858cd9a9c5
michael unanka <michael@michaels-MacBook-Pro.local>
2025-09-04 17:58:54 -0500

    set up project

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+// initializze espress application
+const app = express();
+
+// API endpoints
+app.get('/', (req, res) => res.send('http get request sent to root api endpoint')
+);
+
+// Connection listener
+app.listen(3000, () => console.log('Express server running on port 3000'));
